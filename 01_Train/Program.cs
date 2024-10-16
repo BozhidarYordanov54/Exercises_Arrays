d68c19b7844bf2a89842f7dbaa40321673c6e5ed
BozhidarYordanov54 <bozhidar.yordanov02@gmail.com>
2024-10-16 14:46:57 +0300

    Solved problem 01_Trains

--- 01_Train/Program.cs
+++ 01_Train/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int numOfLines = int.Parse(Console.ReadLine());
+
+int[] peopleInTrainWagons = new int[numOfLines];
+
+for(int i = 0; i < numOfLines; i++)
+{
+    int input = int.Parse(Console.ReadLine());
+
+    peopleInTrainWagons[i] = input;
+}
+
+
+Console.WriteLine($"{string.Join(" ", peopleInTrainWagons)}");
+Console.WriteLine(peopleInTrainWagons.Sum());
